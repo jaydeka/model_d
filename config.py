e09cf62f63572d002c732c62b1fc7e56323eed34
jaydeka <43177728+jaydeka@users.noreply.github.com>
2018-11-15 10:42:20 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+
+folder_path = {
+    "train_dataset" : "E:\Total_Train_dataset/",
+    "test_dataset" : "E:\Total_Test_dataset/"
+}
+
+file_path = {
+    "train_xlsx" : "E:/temp.xlsx",
+    "test_xlsx" : "E:/temp_test.xlsx"
+}
+
+hyperparameter_train = {
+    "training_iters" : 10,
+    "learning_rate" : 0.001,
+    "batch_size" : 1,
+    "no_epochs" : 200,
+    "n_classes" : 3
+}
